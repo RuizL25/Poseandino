a8448282d0ff460257a18cf1891b32432a3be98a
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2022-05-13 21:23:24 -0500

    Add files via upload

---
+++ alertas.js
@@ -0,0 +1,17 @@
+function llamar_alertas(){
+    const alertas = {
+            "0": "Una buena postura es más que pararse derecho para verse mejor. Es una parte importante de su salud a largo plazo.",
+            "1": "El mantener su cuerpo en la posición correcta, ya sea que esté en movimiento o quieto, puede ayudarle a evitar dolores, lesiones y otros problemas de salud.",
+            "2": "Muchas personas pasan demasiado tiempo sentadas, ya sea en el trabajo, la escuela o en casa. Es importante sentarse correctamente y tomar descansos frecuentes. ",
+            "3": "Sabías que... la mala postura afecta la forma correcta de cómo se mueven sus articulaciones. ",
+            "4": "Estando sentado, lo ideal es tener un pequeño soporte o reposapiés en el que podamos descansar nuestros pies. ",
+            "5": "Sabías que... una mala postura durante el desarrollo infantil, puede agravar las desviaciones de columna tanto en el plano lateral (cifosis) como en el frontal (escoliosis). ",
+            "6": "Una buena postura corporal debe ser cómoda y natural. Pero si has empezado a corregir tu postura, es probable que el cambio te resulte incómodo hasta que tu cuerpo no se haya acostumbrado del todo. ",
+            "7": "Sabías que... una mala postura genera un desarrollo de procesos degenerativos- ",
+            "8": "Sabías que... un factor que puede influir en una mala postura es debido a herencia familiar "
+        }
+
+    return alertas;
+}
+
+
